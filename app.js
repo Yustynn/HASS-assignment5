ed1ceb4a6079cfc36a2e8b03ce4dbfe2ea8dd462
Yustynn Panicker <yustynn@gmail.com>
2022-04-05 11:28:36 +0800

    Store total for each bar, and max of total

--- app.js
+++ app.js
@@ -2,34 +2,50 @@ const [WIDTH, HEIGHT] = [1300, 700]
 const COMPONENT_RANGE = [0, 100]
 const COMPONENT_NAMES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
 
+const SPACING = {
+    between: 30,
+    floating: 10
+}
+
 function main() {
     setup()
 
     const data = mkData(3, 3)
+    console.log('data', data)
 }
 
 function mkData(numBars = 3, numComponents = 3) {
-    const result = {
-        data: [],
-        components: COMPONENT_NAMES.slice(0, numComponents),
-        maxs: {},
-    }
+    const data = []
+    const maxs = {}
+    const components = COMPONENT_NAMES.slice(0, numComponents)
+
     for (let i = 0; i < numBars; i++) {
         const datum = {}
-        for (let j = 0; j < numComponents; j++) {
+        let total = 0
+
+        for (const component of components) {
+            // compute value
             const interval = COMPONENT_RANGE[1] - COMPONENT_RANGE[0]
-            const component = COMPONENT_NAMES[j]
             const value = Math.round(Math.random() * interval + COMPONENT_RANGE[0])
             
-            if (result.maxs[component] == undefined) result.maxs[component] = value
-            else result.maxs[component] = Math.max(result.maxs[component], value)
+            // update max
+            if (maxs[component] == undefined) maxs[component] = value
+            else maxs[component] = Math.max(maxs[component], value)
 
+            // set value and update total
             datum[component] = value
+            total += value
         }
-        result.data.push(datum)
+        datum['total'] = total
+
+        // update max total
+        if (maxs['total'] == undefined) maxs['total'] = total
+        else maxs['total'] = Math.max(maxs['total'], total)
+        
+        data.push(datum)
     }
 
-    return result
+    return { components, data, maxs }
 }
 
 
